162fdf2622b2241fba1edd1285094988c7d45ab4
Atinder-Pal <atty.pal46@gmail.com>
2020-10-05 18:34:03 -0600

    displayed header and footer on webpage

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//Set Page Title
+$GLOBALS[ 'pageTitle' ] = 'Filterazzi';
+
+//Show Header
+include dirname(__FILE__).'/templates/header.php';
+
+//Show Footer
+include dirname(__FILE__).'/templates/footer.php';
